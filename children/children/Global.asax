b6fdabe1ad6305ccb4dafbe83a45efbb08006872
Yuzhuzhuzhu <ymaa0049@student.monash.edu>
2022-03-29 22:25:37 +0800

    create web application

---
+++ children/children/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="children.MvcApplication" Language="C#" %>
---
+++ children/children/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(children.Startup))]
+namespace children
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
